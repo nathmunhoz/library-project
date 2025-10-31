c05d4c6cfa1110c36ab214e8337dd929511660cd
nathmunhoz <nmunhoz80@gmail.com>
2025-10-31 11:29:08 -0300

    added grid card for the books and some new style

---
+++ script.js
@@ -0,0 +1,11 @@
+const myLibrary = [];
+
+function Book(title, author, read) {
+	this.title = title;
+	this.author = author;
+	this.read = read;
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
